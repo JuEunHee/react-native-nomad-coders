a7ff746f06e36e14d3b632c4acd6450e6328580d
EUNHEE JU <ehju222@gmail.com>
2025-08-31 19:07:34 +0900

    feature: 스크롤 가능한 day 컴포넌트리스트

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,17 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CameraTest from './components/CameraTest';
+import {
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
   return (
-    // By default react view is already a flex container,
-    // default direction is column
-    // 비율로만 정할 수 있음. 디바이스 사이즈에 따라 다르게 보일 수 있으므로 보통 width, height보다는 flex 사용
-    // 부모에도 flex: 1을 주는게 중요함. 부모를 지우면 자식이 뭐의 5배인지 모르니까 아무것도 보이지 않는다.
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
-      <View style={{ flex: 5, backgroundColor: 'teal' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
+    /**
+     * @see https://reactnative.dev/docs/next/scrollview
+     */
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.weather}
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -19,11 +55,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: 'tomato',
   },
-  text: {
-    fontSize: 48,
+  city: {
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 58,
+    fontWeight: '500',
+  },
+  weather: {
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
+  },
+  temperature: {
+    marginTop: 50,
+    fontSize: 178,
+    fontWeight: '600',
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
