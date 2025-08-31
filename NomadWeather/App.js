65b9939df7e5331a0c9bdcf64f4565e3c73f9f63
EUNHEE JU <ehju222@gmail.com>
2025-08-31 19:35:19 +0900

    feature: 도시 정보를 가져오는 Expo Location api 사용

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,21 +1,54 @@
+import React, { useState, useEffect } from 'react';
 import {
   Dimensions,
   ScrollView,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+import * as Location from 'expo-location';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
+  const [location, setLocation] = useState('Loading...');
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    // 위치 정보 권한 요청
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    
+    if (!granted) {
+      setOk(false);
+    }
+
+    // accuracy: 5 -> 도시 정도의 정확도로 위치 정보를 가져옴
+    const getCurrentLocation = await Location.getCurrentPositionAsync({
+      accuracy: 5
+    });
+    const { latitude, longitude } = getCurrentLocation.coords;
+    const location = await Location.reverseGeocodeAsync({
+      latitude,
+      longitude
+    }, { useGoogleMaps: false });
+
+    console.log(location)
+    console.log(location[0].city ?? location[0].region);
+
+    setLocation(location[0].city ?? location[0].region);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     /**
      * @see https://reactnative.dev/docs/next/scrollview
      */
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{location}</Text>
       </View>
       <ScrollView
         horizontal
@@ -66,8 +99,7 @@ const styles = StyleSheet.create({
     fontSize: 58,
     fontWeight: '500',
   },
-  weather: {
-  },
+  weather: {},
   day: {
     width: SCREEN_WIDTH,
     alignItems: 'center',
